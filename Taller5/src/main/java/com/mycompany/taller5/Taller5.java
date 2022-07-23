4ee221d208ec25c87c20d462372137b9781ef3c2
jmunozc023 <67330305+jmunozc023@users.noreply.github.com>
2022-07-23 16:16:15 -0600

    Parte 1 finalizado

--- Taller5/src/main/java/com/mycompany/taller5/Taller5.java
+++ Taller5/src/main/java/com/mycompany/taller5/Taller5.java
@@ -1,17 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.taller5;
 
-/**
- *
- * @author josep
- */
-public class Taller5 {
+import java.util.Scanner;
 
+
+public class Taller5 {
+    Scanner leer = new Scanner(System.in);
+    String opcion, opcion1;
+    private void seleccion(){
+        do {            
+            System.out.print("Digite una letra para mostrar el dia o digite 'exit' para salir: ");
+            opcion= leer.nextLine();
+            opcion1=opcion.toUpperCase();
+            switch (opcion1) {
+                case "D":
+                    System.out.println("Domingo");
+                    break;
+                case "L":
+                    System.out.println("Lunes");
+                    break;
+                case "K":
+                    System.out.println("Martes");
+                    break;
+                case "M":
+                    System.out.println("Miercoles");
+                    break;
+                case "J":
+                    System.out.println("Jueves");
+                    break;
+                case "V":
+                    System.out.println("Viernes");
+                    break;
+                case "S":
+                    System.out.println("Sabado");
+                    break;
+                case "EXIT":
+                    System.out.println("Gracias");
+                    break;    
+                
+                default:
+                    System.out.println("Letra no permitida");
+            }
+        } while (!"EXIT".equals(opcion1));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Taller5 dia =new Taller5();
+        dia.seleccion();
     }
 }
